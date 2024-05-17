ec8b5531d8dced36159bd742e9d1d153583cbdae
marceloroot <marcelo.lima.gomes.23@gmail.com>
2024-05-17 01:24:40 -0300

    chore: creating the project structuring

---
+++ src/infra/http/ExpressAdpter.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+import cors from 'cors'
+import express, { Errback, Request, Response } from 'express'
+
+import router from '../routes/RoutesEmployee'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(router)
+
+app.use((_: Request, res: Response) => {
+  res.status(404).json({ message: 'Page Not Found' })
+})
+
+app.use((_: Errback, _req: Request, res: Response) => {
+  return res.status(500).json({ message: 'Internal Error' })
+})
+
+export default app
